a008fb8da9a5f8dff7c18e1e194fa7c2563877ab
Jaehyeokk <lionjk92@naver.com>
2021-06-27 22:50:05 +0900

    Setting sidebar for each nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,13 @@
+const routes = ["vue", "javascript", "etc"];
+
+const createSidebar = () => {
+  const sidebar = {};
+  for (const route of routes) {
+    Object.assign(sidebar, require("../" + route));
+  }
+  return sidebar;
+};
+
 module.exports = {
   title: "Jaehyeokk",
   description: "Blog to record",
@@ -5,11 +15,11 @@ module.exports = {
     logo: "https://avatars0.githubusercontent.com/u/67958514?s=120&v=4",
     nav: [
       { text: "Vue", link: "/vue/" },
-      { text: "JavaScript", link: "/javascript/" },
-      { text: "etc.", link: "/etc/" },
+      { text: "Javascript", link: "/javascript/" },
+      { text: "etc.", link: "/etc/markdown/" },
       { text: "Github", link: "https://github.com/Jaehyeokk" },
     ],
-    sidebar: "auto",
+    sidebar: createSidebar(),
     smoothScroll: true,
   },
 };
---
+++ docs/etc/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "/etc/": [
+    {
+      collapsable: false,
+      title: "Markdown",
+      children: ["markdown/"],
+    },
+  ],
+};
